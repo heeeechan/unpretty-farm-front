ae9e064490c4fab6927a59e12adbafd480a03904
siu2388 <swear428@naver.com>
2023-11-22 06:48:28 +0900

    ✨ Feat :  file structure setting start

---
+++ front-test-farm/src/components/Layout/Header.js
@@ -0,0 +1,25 @@
+import React, { Fragment } from 'react';
+
+import style from './styles/Header.module.css';
+import HeaderAlarmButton from './HeaderAlarmButton';
+const Header = (props) => {
+  return (
+    <Fragment>
+      <header className={style.header}>
+        <img
+          src=""
+          alt=""
+        />
+        <ul>
+          <li>못난이 매칭</li>
+          <li>못난이 마켓</li>
+          <li>마이 페이지</li>
+          <li>파머 메이지</li>
+        </ul>
+        <HeaderAlarmButton />
+      </header>
+    </Fragment>
+  );
+};
+
+export default Header;
